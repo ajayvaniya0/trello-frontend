5bed4a2ab38f0d393cd1bcd97052268c0f31f35e
ajayvaniya0 <ajayvaniya54@gmail.com>
2026-09-23 01:27:56 +0530

    Center component

--- src/components/AuthCredentials.tsx
+++ src/components/AuthCredentials.tsx
@@ -1,5 +1,22 @@
+import { Center } from "./Center"
+
 export function AuthCredentials() {
-    return <div>
-        Auth Credentials component 
+    return <div style={{minHeight: "100vh", display: "flex", alignItems: "center"}}>
+        <div style={{width: "100%"}}>
+            <Center>
+                <div style={{fontSize: 30}}>
+                    Log in to Trello
+                </div>
+            </Center>
+            <Center>
+                connect to Trello with:
+            </Center>
+            <Center>
+                <input type="text" placeholder="email"/>
+            </Center>
+            <Center>
+                <input type="text" placeholder="password"/> 
+            </Center>
+        </div>
     </div>
 }
